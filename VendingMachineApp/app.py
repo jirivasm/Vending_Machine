2dd733866df9284adf1b674c515c4b5e4ceb8680
jirivasm <jirivasmo@gmail.com>
2026-03-03 15:03:19 -0600

    Changed postgress db name to vending_machine

--- VendingMachineApp/app.py
+++ VendingMachineApp/app.py
@@ -19,7 +19,7 @@ else:
     db_password = os.environ.get('DB_PASSWORD', 'password')
     db_host = os.environ.get('DB_HOST', 'localhost')
     db_port = os.environ.get('DB_PORT', '5432')
-    db_name = os.environ.get('DB_NAME', 'postgres')
+    db_name = os.environ.get('DB_NAME', 'vending_machine')
 
     # Connection String: postgresql://user:password@host:port/database
     app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
